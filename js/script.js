adc449fa9b056fc36ea279060c803b8226e416f5
Sean <seanreilly123@hotmail.com>
2020-04-18 00:25:51 +1200

    Added footer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
+    $(".open-menu").click(function () {
+        $(".full-menu").show();
+    });
     $(".close-menu").click(function () {
         $(".full-menu").hide();
+    });
+    $("#btn-projects").click(function () {
+        document.getElementById("projects").scrollIntoView();
     });
 });
