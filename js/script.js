1d6b94654cb16ad20f453d5faf6bbb4659af7a5b
Sean <seanreilly123@hotmail.com>
2020-04-26 18:56:17 +1200

    Mobile nav fixed

--- js/script.js
+++ js/script.js
@@ -5,7 +5,11 @@ $(document).ready(function () {
     $(".close-menu").click(function () {
         $(".full-menu").hide();
     });
+    $(".menu-list-item").click(function () {
+        $(".full-menu").hide();
+    });
     $("#btn-projects").click(function () {
         document.getElementById("projects").scrollIntoView();
+        console.log("scroll");
     });
 });
